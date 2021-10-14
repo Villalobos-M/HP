f51f8902ec5c2cda889a22e1bbde43395b392852
Villalobos-M <mar.villalobosrv@gmail.com>
2021-10-14 03:12:17 -0500

    a

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+let idStudent = 0;
+
+const button = document.getElementById("next")
+button.addEventListener( 'click', e => siguienteStudent(e) )
+
+
+
+function getInformacion() {
+    fetch( "http://hp-api.herokuapp.com/api/characters/students" ) //peticion
+    .then( response => response.json() ) //se retorna la respuesta en forma json
+    .then( data => {
+        mostrarStudent( data )  //llamar a funcion y pasar info
+    })
+    .catch( err => console.error(err) ) //Interceptar errores
+}
+
+
+function mostrarStudent(array){
+
+      document.querySelector('#studentImage').setAttribute('src', array[idStudent]['image'])
+      document.querySelector('#name').textContent = array[idStudent].name.toUpperCase()
+      document.querySelector('.casa').textContent = array[idStudent].house
+      document.querySelector('.ancestry').textContent = array[idStudent].ancestry
+      document.querySelector('.patronus').textContent = array[idStudent].patronus
+      document.querySelector('.dateOfBirth').textContent = array[idStudent].dateOfBirth
+      document.querySelector('.actor').textContent = array[idStudent].actor
+}
+
+function siguienteStudent( evento ) {
+    idStudent = idStudent === 78 ? 1 : idStudent + 1 
+
+    getInformacion()
+    evento.stopPropagation()
+}
+
+
+
+
+window.addEventListener( 'DOMContentLoaded', e =>{
+    getInformacion();
+})
